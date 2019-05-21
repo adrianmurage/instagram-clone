1f5c8c0de7f2299f179b35abcfb431b6165fcf53
maina murage <mainamurage21@gmail.com>
2019-05-21 17:57:41 +0300

    Update app models
    Create User model and migrate

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    '''
+    model class to define a user of the app
+    '''
+    username = models.CharField(max_length=20)
+    email = models.CharField(max_length=140)
+    password = models.CharField(max_length=100)
