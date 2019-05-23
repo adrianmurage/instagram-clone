92e0bf47a3398250a7e3fc927f06eb231b61e5db
maina murage <mainamurage21@gmail.com>
2019-05-23 11:47:22 +0300

    Setup authentication backends

---
+++ instagram/authb.py
@@ -0,0 +1,19 @@
+from .models import User
+
+
+class AuthB:
+    '''
+    class to house auth backend for a User
+    '''
+    def authenticate(self, username=None, password=None):
+        try:
+            user = User.objects.get(username=username)
+            return user
+        except:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except:
+            return None
